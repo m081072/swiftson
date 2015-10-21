dd509f42a8fe6794613bbac01da17b35ee9497a2
mikehogan <me@mikehogan.net>
2015-10-21 16:42:45 +0100

    checking type inference

--- test/CalculatorTest.ts
+++ test/CalculatorTest.ts
@@ -2,10 +2,9 @@
 import * as calculator from '../lib/index';
 
 describe('Calculator', () => {
-
     describe('#add', () => {
         it('should add two numbers together', () => {
-            var result : number = calculator.add(2, 3);
+            var result = calculator.add(2, 3);
             if (result !== 5) {
                 throw new Error('Expected 2 + 3 = 5 but was ' + result);
             }
